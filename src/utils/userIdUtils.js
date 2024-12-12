6932815c728e1ae254a849016d251aa01225bf0c
Pawel024 <p.j.rok@student.tudelft.nl>
2024-12-11 23:29:28 -0800

    delete irrelevant stuff

--- src/utils/userIdUtils.js
+++
@@ -1,4 +0,0 @@
-export default function isValidUUID(userProvidedString) {
-    const regex = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89ABab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
-    return regex.test(userProvidedString);
-  }
